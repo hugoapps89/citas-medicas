9aa3fedb40e930324226e353d16f9afa8b522fdf
hugoapps89 <hugoc381@gmail.com>
2026-05-03 16:02:28 -0600

    panel.js

--- panel.js
+++ panel.js
@@ -1,10 +1,30 @@
-const logueado = localStorage.getItem("logueado");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import {
+  getAuth,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-if (logueado !== "true") {
-  window.location.href = "index.html";
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyDn-ETfal7IEjghIaZJlbPRTgyOl3BUcKE",
+  authDomain: "cita-medica-b4c8c.firebaseapp.com",
+  projectId: "cita-medica-b4c8c",
+};
 
-function logout() {
-  localStorage.removeItem("logueado");
-  window.location.href = "index.html";
-}
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// 🔐 VERIFICAR SESIÓN REAL
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "index.html";
+  }
+});
+
+// 🚪 LOGOUT
+window.logout = function () {
+  signOut(auth).then(() => {
+    localStorage.removeItem("logueado");
+    window.location.href = "index.html";
+  });
+};
