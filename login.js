49d7b3679903443d1c4622cca33e029c80f62ec3
hugoapps89 <hugoc381@gmail.com>
2026-05-03 15:22:22 -0600

    login.js

--- login.js
+++ login.js
@@ -14,13 +14,32 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 document.getElementById("btnLogin").addEventListener("click", async () => {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  if (!email || !password) {
+    alert("Completa los campos");
+    return;
+  }
 
   try {
     await signInWithEmailAndPassword(auth, email, password);
     window.location.href = "panel.html";
-  } catch (e) {
-    alert("Credenciales incorrectas");
+  } catch (error) {
+    console.log("ERROR REAL:", error);
+
+    if (error.code === "auth/user-not-found") {
+      alert("Este usuario NO existe en Firebase");
+    } else if (error.code === "auth/wrong-password") {
+      alert("Contraseña incorrecta");
+    } else if (error.code === "auth/invalid-email") {
+      alert("Correo mal escrito");
+    } else if (error.code === "auth/network-request-failed") {
+      alert("Error de conexión (revisa internet o hosting)");
+    } else {
+      alert("Error: " + error.code);
+    }
   }
+
 });
